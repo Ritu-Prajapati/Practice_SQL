76feca54d80aba21a9434aa8d475ae1828832c1c
Ritu Prajapati <130266066+Ritu-Prajapati@users.noreply.github.com>
2024-11-27 23:51:28 +0530

    Create product.sql

---
+++ product.sql
@@ -0,0 +1,11 @@
+#Module: Gross Sales Report: Monthly Product Transactions
+
+-- a. Perform joins to pull product information
+	SELECT s.date, s.product_code, p.product, p.variant, s.sold_quantity 
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        ON s.product_code=p.product_code
+	WHERE 
+            customer_code=90002002 AND 
+    	    get_fiscal_year(date)=2021;
+            
